409d15da8411be21c9fb2ce219fca8dfed942442
Amandeep Singh <dhillonaman21@gmail.com>
2026-09-21 21:48:07 +0530

    Variables Defined

---
+++ variables.js
@@ -0,0 +1,13 @@
+// Different Variables Declared
+const userName = "Amandeep Singh";
+let userId = 18;
+var userEmail = "aman@gmail.com";
+userCity = "Mohali";
+let userState;
+
+// userName = "Deep"; // Due to Const Variable, we do not modify value.
+// console.log(userName);
+userId = 20;
+userEmail = "Deep@gmail.com";
+userCity = "Chandigarh";
+console.table([userId, userEmail, userCity, userState]);
