0e1d1ee1b07dc69f8deacb16604ad96a934608cc
Amandeep Singh <dhillonaman21@gmail.com>
2026-09-23 16:31:49 +0530

    Javascript Data types

---
+++ datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; // Treat all JS Code as newer version
+
+// Data Types:-
+/*1.) Primitive Datatypes
+2.) Non Primitive Datatypes*/
+
+// 1.) Primitive Data types:- Number, Bigint, String, Boolean, Null, Undefinied, Symbol
+console.log(typeof "Amandeep Singh"); // Typeof to check data type of value.
+console.log(typeof null); // Null type is a Object.
+console.log(typeof undefined); // Undefined type is a Undefined.
+
+// 2.) Non Primitive Data types:- 
