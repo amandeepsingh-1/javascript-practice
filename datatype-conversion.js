745e5741a45093a646ddb032ee6eae8f4f903a87
Amandeep Singh <dhillonaman21@gmail.com>
2026-09-23 17:18:00 +0530

    Datatype Conversion in Javascript.

---
+++ datatype-conversion.js
@@ -0,0 +1,22 @@
+let score = "18";
+let valinNumber = Number(score);
+// console.log(typeof (valinNumber)); // String converted into Number.
+
+// Data Type Conversion into Number:-
+// 1.) "33":- 33(Number);
+// 2.) "33abc":- Nan(Not a Number);
+// 3.) true:- 1, false:- 0;
+
+let isLoggedIn = 1;
+let isBooleanLogged = Boolean(isLoggedIn);
+// console.log(typeof (isBooleanLogged));
+
+// Data Type Conversion into Boolean:-
+// 1.) 1:- True
+// 2.) 0:- False
+// 3.) " ": - False
+// 4.) "Aman":- True
+
+let rollNum = 18;
+let strNum = String(rollNum);
+console.log(typeof (strNum));
